f044ca9bf5f29c5ebd2d70038d597219aac7066c
K0IN <thisk0in@gmail.com>
2024-01-18 00:38:16 +0100

    Use the new commands, and structure

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21
 
 require (
 	github.com/akamensky/argparse v1.4.0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,98 +2,59 @@ package main
 
 import (
 	"fmt"
+	"myapp/cli"
 	sandbox "myapp/sandbox"
 	"os"
-	"os/exec"
-	"syscall"
 
 	"github.com/akamensky/argparse"
 )
 
-func createSandboxInsideNamespace(entryCommand string) int {
-	sandboxPaths, err := sandbox.CreateSandboxDirectories()
-	if err != nil {
-		panic(err)
+func executeSandbox(hostname *string) int {
+	if os.Getuid() != 0 || os.Getgid() != 0 {
+		panic("started in namespace mode but not as root")
 	}
-
-	println("Created sandbox directories", sandboxPaths.SandboxDir)
-
-	if err := sandbox.MountDevices(sandboxPaths.RootFsBasePath); err != nil {
-		panic(err)
-	}
-
-	if err := sandbox.MountProc(sandboxPaths.RootFsBasePath); err != nil {
-		panic(err)
-	}
-
-	_ = sandbox.SetSandboxHostname()
-
-	// current dir
-	currentWorkingDir := "/"
-	if workDir, err := os.Getwd(); err == nil {
-		currentWorkingDir = workDir
-	}
-
-	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd \"%s\" && %s", currentWorkingDir, entryCommand))
-	cmd.Dir = "/"
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Chroot: sandboxPaths.RootFsBasePath,
-	}
-	cmd.Env = os.Environ()
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		// panic(err)
-	}
-
-	return cmd.ProcessState.ExitCode()
+	// we are inside the new namespace
+	shell := sandbox.GetPrimaryShell()
+	println("Starting sandbox with shell", shell)
+	sandboxResult := sandbox.CreateSandboxInsideNamespace(shell, *hostname)
+	return sandboxResult
 }
 
 func main() {
-	secret := "sandbox-secret"
-	if len(os.Args) > 1 && os.Args[1] == secret {
-		if os.Getuid() != 0 || os.Getgid() != 0 {
-			panic("started in namespace mode but not as root")
-		}
-		// we are inside the new namespace
-		shell := sandbox.GetPrimaryShell()
-		println("Starting sandbox with shell", shell)
-		sandboxResult := createSandboxInsideNamespace(shell)
-		os.Exit(sandboxResult)
-		return
-	}
-
 	parser := argparse.NewParser("sandbox", "run a command in a sandbox")
-	tryCommand := parser.NewCommand("try", "execute a command inside a sandbox and review the changes")
-	// --disable-network
-	// --disable-env
-	// --env key=value
-
+	tryParser, tryArguments := cli.GetTryCommandParser(parser)
+	diffParser := cli.GetDiffCommandParser(parser)
+	statusParser := cli.GetStatusCommandParser(parser)
+	listParser := cli.GetListCommandParser(parser)
 	// skipDiff := tryCommand.Flag("s", "skip-diff", &argparse.Options{Required: false, Default: false, Help: "skip diff"})
 	// workDir := tryCommand.String("c", "workdir", &argparse.Options{Required: false, Help: "workdir"})
 	// args := parser.StringPositional(&argparse.Options{Required: true, Help: "command to execute"})
-
-	// skipDiff := tryCommand.Flag("s", "skip-diff", &argparse.Options{Required: false, Default: false, Help: "skip diff"})
-
-	// diffCommand := parser.NewCommand("diff", "show a diff of all files")
-	// addCommand := parser.NewCommand("add", "add files from the sandbox to be committed")
-	// --restore staged
-
-	// status := parser.NewCommand("status", "show the status of the sandbox")
-
 	// commit := parser.NewCommand("commit", "commit the previously added files to disk")
-
 	// rm := parser.NewCommand("rm", "forcefully remove all sandbox files")
 
-	// diff := parser.NewCommand("diff", "show a diff of all files")
+	// do NOT use this command directly, use try instead, else you will run the container code on your host!
+	execute := parser.NewCommand("sandbox-entry", argparse.DisableDescription)
+	sandboxHostName := execute.String("", "hostname", &argparse.Options{Required: true, Help: "hostname"})
 
 	if err := parser.Parse(os.Args); err != nil {
 		fmt.Print(parser.Usage(err))
-		os.Exit(1)
-	}
-
-	if tryCommand.Happened() {
-		sandbox.ForkSelfIntoNewNamespace(sandbox.SandboxConfig{AllowNetwork: false, AllowEnv: true, Arguments: os.Args[1:]}) // this will call us again with an argument
+	} else if execute.Happened() {
+		sandboxResult := executeSandbox(sandboxHostName)
+		os.Exit(sandboxResult)
+	} else if diffParser.Happened() {
+		if err := cli.ExecuteDiffCommand(); err != nil {
+			panic(err)
+		}
+	} else if statusParser.Happened() {
+		if err := cli.ExecuteStatusCommand(); err != nil {
+			panic(err)
+		}
+	} else if listParser.Happened() {
+		if err := cli.ExecuteListCommand(); err != nil {
+			panic(err)
+		}
+	} else if tryParser.Happened() {
+		executeResult := cli.ExecuteTryCommand(tryArguments)
+		os.Exit(executeResult)
 	}
 }
