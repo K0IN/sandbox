79b964e6d446e964e14aa4a847b42db29dacf184
K0IN <thisk0in@gmail.com>
2024-01-19 00:08:56 +0100

    Remove comments

--- cli/diff.go
+++ cli/diff.go
@@ -1,17 +1,3 @@
-/*
-	func main() {
-	    fileInfo, _ := os.Stdout.Stat()
-	    if (fileInfo.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
-	        fmt.Println("Output is being displayed in a terminal")
-	        // Output is being displayed in a terminal
-	        // Your logic for terminal output
-	    } else {
-	        fmt.Println("Output is being piped to another program")
-	        // Output is being piped to another program
-	        // Your logic for piped output
-	    }
-	}
-*/
 package cli
 
 import (
