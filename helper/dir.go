328855b4aece21dee58b77c05b1f943e9f071e2b
K0IN <thisk0in@gmail.com>
2024-01-27 16:21:09 +0100

    Create dir.go

---
+++ helper/dir.go
@@ -0,0 +1,10 @@
+package helper
+
+import "os"
+
+func CreateDirIfNotExists(dirPath string) error {
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		return os.MkdirAll(dirPath, 0755)
+	}
+	return nil
+}
