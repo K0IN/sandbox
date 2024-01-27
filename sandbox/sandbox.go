47cff18deac1401fdad5b666fa73148dcba70b54
K0IN <thisk0in@gmail.com>
2024-01-27 16:21:05 +0100

    Update sandbox.go
    * Add correct path
    * Add sandbox returns there fs path

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -35,7 +35,7 @@ func CreateSandboxAt(sandboxBaseDir string) (*Sandbox, error) {
 		return nil, fmt.Errorf("failed to create overlay: %w", err)
 	}
 
-	specialMounts, err := CreateSpecialMounts(overlay.GetMountPath())
+	specialMounts, err := CreateSpecialMounts(overlay.upperDir)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create special mounts: %w", err)
 	}
@@ -86,29 +86,23 @@ func (s *Sandbox) LoadSandbox(sandboxId string) (*Sandbox, error) {
 	return LoadSandboxFrom(sandboxDir)
 }
 
-func (s *Sandbox) Execute(command string, params SandboxParams) (returnCode int, err error) {
-	// todo mount devices'
+func (s *Sandbox) Execute(command string, params SandboxParams) (returnCode int, sandboxDir string, err error) {
 	if err := s.overlayFs.Mount(); err != nil {
-		return 0, fmt.Errorf("failed to mount overlay: %w", err)
+		return 0, "", fmt.Errorf("failed to mount overlay: %w", err)
 	}
 	defer s.overlayFs.UnMount()
 
 	if err := s.specialMounts.Mount(); err != nil {
-		return 0, fmt.Errorf("failed to mount special mounts: %w", err)
+		return 0, "", fmt.Errorf("failed to mount special mounts: %w", err)
 	}
-	defer func() {
-		err := s.specialMounts.Unmount()
-		if err != nil {
-			fmt.Println("failed to unmount special mounts:", err)
-		}
-	}()
+	defer s.specialMounts.Unmount()
 
 	currentWorkingDir := "/"
-	// if workDir, err := os.Getwd(); err == nil {
-	// 	currentWorkingDir = workDir
-	// }
+	if workDir, err := os.Getwd(); err == nil {
+		currentWorkingDir = workDir
+	}
 
-	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd \"%s\" && %s", currentWorkingDir, command))
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd \"%s\" ; hostname sandbox; %s", currentWorkingDir, command))
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC | syscall.CLONE_NEWUSER,
 		UidMappings: []syscall.SysProcIDMap{
@@ -147,7 +141,7 @@ func (s *Sandbox) Execute(command string, params SandboxParams) (returnCode int,
 	cmd.Stderr = os.Stderr
 	_ = cmd.Run()
 
-	return cmd.ProcessState.ExitCode(), nil
+	return cmd.ProcessState.ExitCode(), s.overlayFs.BaseDir, nil
 }
 
 func (s *Sandbox) DeleteSandbox() error {
